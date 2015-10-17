beb838897477f3ed86884f41098a39f123211332
stevenpslade <stevenpslade@gmail.com>
2015-10-17 23:52:37 +0000

    ability to post message through form

--- app/actions.rb
+++ app/actions.rb
@@ -6,5 +6,18 @@ end
 get '/messages' do
   @messages = Message.all
   erb :'messages/index'
-  erb: 'messages/new'
+end
+
+ get '/messages/new' do
+   erb :'messages/new'
+ end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author:  params[:author]
+  )
+  @message.save
+  redirect '/messages'
 end
